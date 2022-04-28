be9ba8c4cb0cbc7a59a25da29dacf1cd6daf68d2
devAYZ <afatokimi@getcarbon.co>
2022-04-28 15:52:33 +0100

    add project and pod file

---
+++ Pods/Target Support Files/Pods-test-r-swift-test-r-swiftUITests/Pods-test-r-swift-test-r-swiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_r_swift_test_r_swiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_test_r_swift_test_r_swiftUITests
+@end
---
+++ Pods/Target Support Files/Pods-test-r-swiftTests/Pods-test-r-swiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_r_swiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_test_r_swiftTests
+@end
